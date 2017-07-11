741f9c73a81e4c6a2406addac504f261740b0f16
Amir Hossein Babaeian <babaeian@rayatak.ir>
2017-07-11 15:23:24 +0430

    js,css folders structure change

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -4,8 +4,8 @@
         test
     </title>
 
-    <link type="text/css" rel="stylesheet" href="amirhb/js/jsgrid/jsgrid.min.css" />
-    <link type="text/css" rel="stylesheet" href="amirhb/js/jsgrid/jsgrid-theme.min.css" />
+    <link type="text/css" rel="stylesheet" href="amirhb/css/jsgrid/jsgrid.min.css" />
+    <link type="text/css" rel="stylesheet" href="amirhb/css/jsgrid/jsgrid-theme.min.css" />
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="amirhb/js/jsgrid/jsgrid.min.js"></script>
