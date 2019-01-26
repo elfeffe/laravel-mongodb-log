4e423d5344f46ae0adc2000d610dd75180da6f78
Amir Hossein Babaeian <babaeian@rayatak.ir>
2019-01-26 15:24:27 +0330

    laravel 5.6.x handler

---
+++ src/LogHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Amirhb\LaravelMongodbLog;
+
+use Monolog\Logger;
+
+class LogHandler
+{
+    /**
+     * Create a custom Monolog instance.
+     *
+     * @param  array  $config
+     * @return \Monolog\Logger
+     */
+    public function __invoke(array $config)
+    {
+        $logger = new Logger('mongodb');
+        $logger->pushHandler(new EloquentHandler());
+        $logger->pushProcessor(new RequestProcessor());
+
+        return $logger;
+    }
+}
