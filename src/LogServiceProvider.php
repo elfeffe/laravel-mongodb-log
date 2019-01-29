99269a9606f49a5250eb7e8238f2b9f110f0e9fd
Amir Hossein Babaeian <babaeian@rayatak.ir>
2019-01-29 16:17:11 +0330

    typo

--- src/LogServiceProvider.php
+++ src/LogServiceProvider.php
@@ -14,7 +14,7 @@ class LogServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/config.php' => config_path('mongodb-log'),
+            __DIR__.'/config.php' => config_path('mongodb-log.php'),
         ], 'config');
     }
 
