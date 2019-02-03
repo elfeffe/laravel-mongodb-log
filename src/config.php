e2c951c05a863f0b70d34c0306f94f05c460ce43
Amir Hossein Babaeian <babaeian@rayatak.ir>
2019-02-03 12:37:03 +0330

    configs for envs 2

--- src/config.php
+++ src/config.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'connection' => 'mongodb',
-    'collection' => 'logs',
+    'connection' => env('MONGODB_CONNECTION' ,'mongodb'),
+    'collection' => env('MONGODB_LOG_COLLECTION' ,'logs'),
 ];
